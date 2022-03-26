35f1d8a072d5a4865b6a28446dd09a7e632ae45b
Luca De Santis <lucaroma2705@gmail.com>
2022-03-26 22:33:18 +0100

    add sidebar with default projects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+
+const sideBar = document.querySelector(".sidebar");
+const menu = document.querySelector(".menu-icon");
+const main = document.querySelector("main");
+
+menu.addEventListener("click", () => {
+    sideBar.classList.toggle("hide");
+    main.classList.toggle("translate");
+});
