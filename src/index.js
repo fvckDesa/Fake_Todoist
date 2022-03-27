fa7e7d7146095ecd0a3971d89100ec33ebe96aa8
Luca De Santis <lucaroma2705@gmail.com>
2022-03-27 19:30:59 +0200

    added user projects in the sidebar and split the CSS code into different files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import './style.css';
+import './styles/header.css';
+import './styles/sidebar.css';
+import './styles/main.css';
 
-const sideBar = document.querySelector(".sidebar");
+
 const menu = document.querySelector(".menu-icon");
-const main = document.querySelector("main");
 
-menu.addEventListener("click", () => {
+menu.addEventListener("click", moveSideBar);
+
+function moveSideBar() {
+    const sideBar = document.querySelector(".sidebar");
+    const main = document.querySelector("main");
+
     sideBar.classList.toggle("hide");
     main.classList.toggle("translate");
-});
+}
+
+//#region test for user projects
+const template = document.querySelector("template").content;
+const container = document.querySelector(".user-container");
+
+for(let i = 0; i < 10; i++){
+    const copy = template.cloneNode(true);
+    copy.querySelector(".sidebar-item-text-name").innerText = "Prova";
+    copy.querySelector(".sidebar-item-text-number").innerText = i;
+    container.appendChild(copy);
+}
+
+//#endregion
