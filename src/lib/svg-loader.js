7a4be85d3e49424fa94b043c391da7199a705b12
Luca De Santis <lucaroma2705@gmail.com>
2022-07-15 16:43:13 +0200

    chace save in local storage

--- src/lib/svg-loader.js
+++ src/lib/svg-loader.js
@@ -1,46 +1,49 @@
 class SvgLoader extends HTMLElement {
+  #src = "";
   constructor() {
     super();
     // create shadow root
     this.attachShadow({ mode: "open" });
-    // display: flex on svg-element to give it the same width and height as the internal svg 
-    this.style.display = "flex";
     // default empty svg
     this.svg = document.createElement("svg");
-    // render svg
-    this.render(this.getAttribute("src"));
   }
 
   render(src) {
-    if(!src) return;
-    getSVG(src).then((svg) => {
-      const scriptRegex = /<script>(.|\n|\r)*<\/script>/g;
-      // add svg to shadow root (not script)
-      this.shadowRoot.innerHTML = svg.replace(scriptRegex, "");
-      // set svg property to svg in shadow root
-      this.svg = this.shadowRoot.querySelector("svg");
-      // get script in svg file
-      let svgScript = svg.match(scriptRegex);
-      if(svgScript) {
-        // run the script
-        svgScript = svgScript[0].replace(/<\/?script>/g, "");
-        eval(`const shadowRoot = document.querySelector("#today-icon").shadowRoot;\n ${svgScript}`);
-      }
-      // add attributes to svg
-      for (const { name, value } of this.attributes) {
-        if(name.match(/^svg:/)) this.svg.setAttribute(name.slice(4), value);
-        if(name === "width" || name === "height") this.svg.setAttribute(name, value);
-      }
-    });
+    if (!src) return;
+    getSVG(src)
+      .then((svg) => {
+        const scriptRegex = /<script>(.|\n|\r)*<\/script>/g;
+        // add svg to shadow root (not script)
+        this.shadowRoot.innerHTML = svg.replace(scriptRegex, "");
+        // set svg property to svg in shadow root
+        this.svg = this.shadowRoot.querySelector("svg");
+        // get script in svg file
+        let svgScript = svg.match(scriptRegex);
+        if (svgScript) {
+          // run the script
+          svgScript = svgScript[0].replace(/<\/?script>/g, "");
+          eval(
+            `const shadowRoot = document.querySelector("#today-icon").shadowRoot;\n ${svgScript}`
+          );
+        }
+        // add attributes to svg
+        for (const { name, value } of this.attributes) {
+          if (name.match(/^svg:/)) this.svg.setAttribute(name.slice(4), value);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   static get observedAttributes() {
-    return ["src", "svg:class", "svg:id", "width", "height"];
+    return ["src", "svg:class", "svg:id"];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     switch (name) {
       case "src":
+        this.#src = newValue;
         this.render(newValue);
         break;
       case "svg:class":
@@ -49,38 +52,39 @@ class SvgLoader extends HTMLElement {
       case "svg:id":
         this.svg.setAttribute("id", newValue);
         break;
-      case "width":
-        this.style.width = newValue;
-        this.svg.setAttribute("width", newValue);
-        break;
-      case "height":
-        this.style.height = newValue;
-        this.svg.setAttribute("height", newValue);
-        break;
     }
+  }
+
+  connectedCallback() {
+    this.render(this.#src);
+    // display: flex on svg-element to give it the same width and height as the internal svg
+    this.style.display = "flex";
   }
 
   set src(newSrc) {
     this.setAttribute("src", newSrc);
   }
 
-  set width(newWidth) {
-    this.setAttribute("width", newWidth);
-  }
-  
-  set height(newHeight) {
-    this.setAttribute("height", newHeight);
+  get src() {
+    return this.#src;
   }
 }
 
+const cache = localStorage.getItem("svg-cache")
+  ? JSON.parse(localStorage.getItem("svg-cache"))
+  : {};
+
 async function getSVG(url) {
+  if (cache[url]) return cache[url];
   let svg;
   try {
     svg = await (await fetch(url)).text();
+    cache[url] = svg;
   } catch (err) {
     console.error(err);
     throw new Error(err);
   }
+  localStorage.setItem("svg-cache", JSON.stringify(cache));
   return svg;
 }
 
