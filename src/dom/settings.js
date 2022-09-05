da71d7d4df931679245409c279caa6625cccf413
Luca De Santis <lucaroma2705@gmail.com>
2022-09-05 22:03:24 +0200

    add message if settings change isn't save on close

--- src/dom/settings.js
+++ src/dom/settings.js
@@ -1,7 +1,17 @@
-import { settings, settingsContainer, setting, settingsList, settingContent, settingName, settingFooter, closeSettingsBtn } from "./elements";
+import {
+  settings,
+  settingsContainer,
+  setting,
+  settingsList,
+  settingContent,
+  settingName,
+  settingFooter,
+  closeSettingsBtn,
+} from "./elements";
 import getSettingPage from "./settings-pages";
 import { settingsType } from "../settings/data";
 import appSettings, { changeSettings } from "../settings";
+import activeInfoPopUp from "./info-pop-up";
 
 let handleSubmit = () => {};
 
@@ -11,20 +21,21 @@ let isChange = false;
 settingsContainer.addEventListener("click", closeSettings);
 
 settings.addEventListener("click", (e) => {
-    e.stopPropagation();
+  e.stopPropagation();
 });
 
 setting.addEventListener("submit", (e) => {
-    e.preventDefault();
-    settingFooter.classList.remove("active");
-    for(const { setting, value } of handleSubmit()) {
-        changeSettings(setting, value);
-    }
+  e.preventDefault();
+  settingFooter.classList.remove("active");
+  for (const { setting, value } of handleSubmit()) {
+    changeSettings(setting, value);
+  }
+  isChange = false;
 });
 
 setting.addEventListener("reset", (e) => {
-    e.preventDefault();
-    resetSettings();
+  e.preventDefault();
+  resetSettings();
 });
 
 closeSettingsBtn.addEventListener("click", closeSettings);
@@ -32,57 +43,71 @@ closeSettingsBtn.addEventListener("click", closeSettings);
 createSettings();
 
 export default function activeSettings(setting) {
-    settingsContainer.classList.remove("hidden");
+  settingsContainer.classList.remove("hidden");
 
-    settingsList.querySelector(`[data-setting="${setting}"`).click();
+  settingsList.querySelector(`[data-setting="${setting}"`).click();
 }
 
 function createSettings() {
-    settingsList.replaceChildren(
-        ...settingsType.map(({ setting, icon }) => {
-            const settingsItem = document.createElement("li");
-            settingsItem.classList.add("settings-item");
-            settingsItem.dataset.setting = setting;
+  settingsList.replaceChildren(
+    ...settingsType.map(({ setting, icon }) => {
+      const settingsItem = document.createElement("li");
+      settingsItem.classList.add("settings-item");
+      settingsItem.dataset.setting = setting;
 
-            const iconEl = document.createElement("svg-loader");
-            iconEl.classList.add("settings-item-icon");
-            iconEl.src = icon;
+      const iconEl = document.createElement("svg-loader");
+      iconEl.classList.add("settings-item-icon");
+      iconEl.src = icon;
 
-            const name = document.createElement("h3");
-            name.innerText = setting;
+      const name = document.createElement("h3");
+      name.innerText = setting;
 
-            settingsItem.append(iconEl, name);
+      settingsItem.append(iconEl, name);
 
-            settingsItem.addEventListener("click", () => {
-                isChange = false;
-                settingSelect = setting;
-                const { page, getValue } = getSettingPage(setting, handleChange);
+      settingsItem.addEventListener("click", () => {
+        isChange = false;
+        settingSelect = setting;
+        const { page, getValue } = getSettingPage(setting, handleChange);
 
-                handleSubmit = getValue;
-                settingContent.replaceChildren(page);
+        handleSubmit = getValue;
+        settingContent.replaceChildren(page);
 
-                settingsList.querySelector(".active")?.classList.remove("active");
-                settingsItem.classList.add("active");
+        settingsList.querySelector(".active")?.classList.remove("active");
+        settingsItem.classList.add("active");
 
-                settingName.innerText = setting;
-            });
+        settingName.innerText = setting;
+      });
 
-            return settingsItem;
-        })
-    );
+      return settingsItem;
+    })
+  );
 }
 
 function resetSettings() {
-    settingFooter.classList.remove("active");
-    if(settingSelect === "theme") changeSettings("theme", appSettings["theme"]);
+  settingFooter.classList.remove("active");
+  if (settingSelect === "theme") changeSettings("theme", appSettings["theme"]);
+  isChange = false;
 }
 
 function handleChange(name, value) {
-    isChange = appSettings[name] !== value;
-    settingFooter.classList.toggle("active", isChange);
+  isChange = appSettings[name] !== value;
+  settingFooter.classList.toggle("active", isChange);
 }
 
 function closeSettings() {
+  if(!isChange) {
     settingsContainer.classList.add("hidden");
     resetSettings();
+    return;
+  }
+  activeInfoPopUp(
+    "Discard changes?",
+    "The changes you've made won't be saved.",
+    "Discard",
+    [],
+    () => {
+      settingsContainer.classList.add("hidden");
+      resetSettings();
+    }
+  );
 }
